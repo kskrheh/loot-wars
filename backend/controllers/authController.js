f26dda88453fdd5dc1804335a38050bc2d153b58
Costa <kskrheh@gmail.com>
2022-06-21 20:46:23 +0300

    another deploy

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -64,41 +64,41 @@ async function logoutUser(req, res) {
 async function getUserInfo(req, res) {
   let weapons;
   let user;
+  const { username } = req.session.user;
   try {
-    const { username } = req.session.user;
     user = await User.findOne({
       where: {
         username,
       },
     });
-    try {
-      weapons = await UserWeapon.findAll({
-        where: {
-          user_id: user.id,
-        },
-        include: {
-          model: Weapon,
-          attributes: [
-            'id', 'ATK', 'DEF', 'title', 'quality',
-          ],
-        },
-        attributes: [
-          'id',
-          ['weapon_id', 'weapon_id'],
-          'wear',
-          [sequelize.col('Weapon.ATK'), 'ATK'],
-          [sequelize.col('Weapon.DEF'), 'DEF'],
-          [sequelize.col('Weapon.title'), 'title'],
-          [sequelize.col('Weapon.quality'), 'quality'],
-        ],
-        raw: true,
-      });
-      res.json({ name: user.username, weapons, energy: user.energy });
-    } catch (err) {
-      res.json(user.username);
-    }
   } catch (err) {
     res.send(err.message);
+  }
+  try {
+    weapons = await UserWeapon.findAll({
+      where: {
+        user_id: user.id,
+      },
+      include: {
+        model: Weapon,
+        attributes: [
+          'id', 'ATK', 'DEF', 'title', 'quality',
+        ],
+      },
+      attributes: [
+        'id',
+        ['weapon_id', 'weapon_id'],
+        'wear',
+        [sequelize.col('Weapon.ATK'), 'ATK'],
+        [sequelize.col('Weapon.DEF'), 'DEF'],
+        [sequelize.col('Weapon.title'), 'title'],
+        [sequelize.col('Weapon.quality'), 'quality'],
+      ],
+      raw: true,
+    });
+    res.json({ name: user.username, weapons, energy: user.energy });
+  } catch (err) {
+    res.json(user.username);
   }
 }
 module.exports = {
