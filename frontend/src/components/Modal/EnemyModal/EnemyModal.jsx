19520c84d1f2eea055d44a5cb771b8820ecda52d
Anastasia <dekamoru@gmail.com>
2022-06-21 19:38:45 +0300

    start fight

--- frontend/src/components/Modal/EnemyModal/EnemyModal.jsx
+++ frontend/src/components/Modal/EnemyModal/EnemyModal.jsx
@@ -2,19 +2,30 @@ import React from 'react';
 import './EnemyModal.css'
 import {useSelector} from "react-redux";
 import Weapon from "../../Loot/Weapon/Weapon";
+import {useState} from "react";
+import FightModal from "../FightModal/FightModal";
 
 const EnemyModal = () => {
+  const [fight, setFight] = useState(false)
   const enemy = useSelector((state) => state.enemy.enemy);
+
+  const handleClick = () => {
+    setFight(true)
+  }
   return (
     <>
-      <div className = 'app'>
-        <span>{`ATK ${enemy.weapons.reduce((sumAtk, weapon) => sumAtk + weapon.ATK, 0)}`} </span>
-        <span>{`DEF ${enemy.weapons.reduce((sumDef, weapon) => sumDef + weapon.DEF, 0)}`}</span>
-        {
-        enemy.weapons.map((weapon, index) => <Weapon weapon={weapon} key={index}/>)
+      { fight ?
+        <FightModal/>
+         :
+        <div className='app'>
+          <span>{`ATK ${enemy.weapons.reduce((sumAtk, weapon) => sumAtk + weapon.ATK, 0)}`} </span>
+          <span>{`DEF ${enemy.weapons.reduce((sumDef, weapon) => sumDef + weapon.DEF, 0)}`}</span>
+          {
+            enemy.weapons.map((weapon, index) => <Weapon weapon={weapon} key={index}/>)
+          }
+          <button onClick={handleClick}>Бить</button>
+        </div>
       }
-        <button>Бить</button>
-      </div>
     </>
   );
 }
