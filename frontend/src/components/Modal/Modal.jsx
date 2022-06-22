e9e5f2cb6bc834208f4ee19dd5bbfee304d3644d
Anton <anton.yasinyuk@mail.ru>
2022-06-22 11:00:35 +0300

    d

--- frontend/src/components/Modal/Modal.jsx
+++ frontend/src/components/Modal/Modal.jsx
@@ -5,11 +5,11 @@ import styles from'./Modal.module.css'
 const Modal = ({active, setActive, children}) => {
   return (
     <div
-      className={styles.active ? "modal active" : "modal"}
+      className={active ? "modal active" : "modal"}
       onClick={() => setActive(false)}
     >
       <div
-        className={styles.active ? "modal__content active" : "modal__content"}
+        className={active ? "modal__content active" : "modal__content"}
         onClick={(e) => e.stopPropagation()} //чтоб не закрывалось при нажатии на модалку
       >
         {children}
