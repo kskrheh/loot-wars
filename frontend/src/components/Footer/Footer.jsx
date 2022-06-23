58123561cd38fe97d7db4ebd2c77d2de1bc96eed
Anton <anton.yasinyuk@mail.ru>
2022-06-23 22:22:18 +0300

    ++

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -1,39 +1,55 @@
-import { Link } from 'react-router-dom';
-import styles from './Footer.module.css';
-import homeSvg from '../../img/svg/house.svg';
-import showelSvg from '../../img/svg/showel.svg';
-import bladesSvg from '../../img/svg/bladesLarge.svg';
-import chatSvg from '../../img/svg/chat.svg';
-import bookSvg from '../../img/svg/book.svg';
+import { Link } from "react-router-dom";
+import styles from "./Footer.module.css";
+import homeSvg from "../../img/svg/house.svg";
+import showelSvg from "../../img/svg/showel.svg";
+import bladesSvg from "../../img/svg/bladesLarge.svg";
+import chatSvg from "../../img/svg/chat.svg";
+import bookSvg from "../../img/svg/book.svg";
 
 function Footer() {
-
   return (
     <footer>
       <nav>
         <ul className={styles.ul_footer}>
-          <li className={styles.li}><Link to="/">
-            <img alt={''} src={homeSvg}/>
-          </Link></li>
+          <li className={styles.li}>
+            <Link to="/">
+              <div className={styles.colorites}>
+                {" "}
+                <img alt={""} src={homeSvg} />
+              </div>
+            </Link>
+          </li>
 
-          <li className={styles.li}><Link to="/loot">
-            <img alt={''} src={showelSvg}/>
-          </Link></li>
+          <li className={styles.li}>
+            <Link to="/loot">
+              <div className={styles.colorites}>
+                <img alt={""} src={showelSvg} />
+              </div>
+            </Link>
+          </li>
 
-          <li className={styles.li}><Link to="/arena">
-            <img alt={''} src={bladesSvg}/>
-          </Link></li>
+          <li className={styles.li}>
+            <Link to="/arena">
+              <div className={styles.colorites}>
+                <img alt={""} src={bladesSvg} />
+              </div>
+            </Link>
+          </li>
 
           {/*<li className={styles.li}><Link to="/telegram">*/}
           {/*  <img alt={''} src={chatSvg}/>*/}
           {/*</Link></li>*/}
 
-          <li className={styles.li}><Link to="/history">
-            <img alt={''} src={bookSvg}/>
-          </Link></li>
+          <li className={styles.li}>
+            <Link to="/history">
+              <div className={styles.colorites}>
+                <img alt={""} src={bookSvg} />
+              </div>
+            </Link>
+          </li>
         </ul>
       </nav>
-    </footer >
+    </footer>
   );
 }
 
